059c4eb836afce196eb887d49102152e2ef6886e
badlogic <contact@badlogicgames.com>
2012-03-29 12:25:15 +0200

    removed ikvm-natives/build

--- native/ikvm-natives/build/ikvm-natives.build/Release-iphonesimulator/ikvm-natives.build/Objects-normal/i386/jni.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/badlogic/workspace/libgdx/backends/gdx-backend-iosmonotouch/ikvm/ikvm-monotouch/native/ikvm-natives/ikvm-natives/../../jni.c \
-  /Users/badlogic/workspace/libgdx/backends/gdx-backend-iosmonotouch/ikvm/ikvm-monotouch/native/ikvm-natives/ikvm-natives/../../jni.h
--- native/ikvm-natives/build/ikvm-natives.build/Release-iphonesimulator/ikvm-natives.build/Objects-normal/i386/os.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/badlogic/workspace/libgdx/backends/gdx-backend-iosmonotouch/ikvm/ikvm-monotouch/native/ikvm-natives/ikvm-natives/../../os.c \
-  /Users/badlogic/workspace/libgdx/backends/gdx-backend-iosmonotouch/ikvm/ikvm-monotouch/native/ikvm-natives/ikvm-natives/../../jni.h
