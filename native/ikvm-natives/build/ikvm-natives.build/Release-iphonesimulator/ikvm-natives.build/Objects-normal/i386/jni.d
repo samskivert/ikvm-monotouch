4739bdddd4ff6b0028fe72be4ef107d4ea0f30fc
badlogic <contact@badlogicgames.com>
2012-03-29 12:06:58 +0200

    xcode project for native lib, to lazy to figure out cli version

---
+++ native/ikvm-natives/build/ikvm-natives.build/Release-iphonesimulator/ikvm-natives.build/Objects-normal/i386/jni.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/badlogic/workspace/libgdx/backends/gdx-backend-iosmonotouch/ikvm/ikvm-monotouch/native/ikvm-natives/ikvm-natives/../../jni.c \
+  /Users/badlogic/workspace/libgdx/backends/gdx-backend-iosmonotouch/ikvm/ikvm-monotouch/native/ikvm-natives/ikvm-natives/../../jni.h
