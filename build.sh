baa2baab93d5fa20841c01570a4a61e71ce28e4c
Michael Bayne <mdb@samskivert.com>
2013-09-06 15:43:48 -0700

    Updated to work with Xamarin 6.4 / Mono 3.x.
    Now that I understand the twisty maze of system DLL dependencies a bit more, I was able to
    eliminate the five phase build process. However, getting NAnt to work with Mono 3.x (specifically
    the .NET 4.5 profile) is more complicated. Hopefully NAnt will ship an update some time soon that
    supports Mono 3.x. Not likely since their last update was over a year ago...

--- build.sh
+++ build.sh
@@ -3,8 +3,4 @@
 set -e
 
 export PKG_CONFIG_PATH=/Library/Frameworks/Mono.framework/Versions/Current/lib/pkgconfig
-nant phase1
-nant -t:moonlight-2.0 phase2
-nant phase3
-nant -t:moonlight-2.0 phase4
-nant phase5
+nant -t:mono-4.5
